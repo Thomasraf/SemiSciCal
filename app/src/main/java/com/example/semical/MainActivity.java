15a7e45ea3b3d39456419054cfadbb062c10a017
Thomasraf <thomas_cruz@dlsu.edu.ph>
2019-10-15 21:53:30 +0800

    Updated

--- app/src/main/java/com/example/semical/MainActivity.java
+++ app/src/main/java/com/example/semical/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.semical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView TextViewValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextViewValue = (TextView) findViewById(R.id.TextViewCurrentValue);
     }
+
+    public void calculateOnClick(View view) {
+        Button pressed = (Button) findViewById(view.getId());
+        String value = pressed.getText().toString();
+        TextViewValue.setText(value + "");
+    }
+
+    public void toOps(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String number1 = TextViewValue.getText().toString();
+        intent.putExtra("NUM1", number1);
+
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/semical/SecondActivity.java
+++ app/src/main/java/com/example/semical/SecondActivity.java
@@ -2,13 +2,36 @@ package com.example.semical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView TextViewCurrentValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextViewCurrentValue = findViewById(R.id.TextViewCurrentValue);
+
+        String num1 = getIntent().getStringExtra("NUM1");
+        TextViewCurrentValue.setText(num1);
+    }
+
+    public void goBack(View view) {
+        Button opsBtn = (Button) findViewById(view.getId());
+        String operand = opsBtn.getText().toString();
+
+        String num1 = getIntent().getStringExtra("NUM1");
+        TextViewCurrentValue.setText(num1 + operand);
+
+        Intent intentBack = new Intent(this, MainActivity.class);
+        intentBack.putExtra("OPS", operand);
     }
 }
